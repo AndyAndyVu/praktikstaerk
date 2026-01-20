01cacb142b38081cfb0931dba3eaa9f2353d1774
Andy <andyandyvu@gmail.com>
2026-01-20 16:52:20 +0100

    add navigation helt færdig

--- mit-nuxt-projekt/app/components/Navdesk.vue
+++ mit-nuxt-projekt/app/components/Navdesk.vue
@@ -1,5 +1,10 @@
 <template>
-  <header>
+  <header class="header">
+    <div class="logo">
+      <NuxtLink to="/">
+        <img src="/img/HAPS-logo.png" alt="HAPS logo" />
+      </NuxtLink>
+    </div>
     <nav aria-label="Hovednavigation">
       <ul class="nav-list">
         <li>
@@ -12,37 +17,64 @@
           <NuxtLink to="/kontakt">Kontakt</NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/login">Login/</NuxtLink>
-
-          <NuxtLink to="/opret-bruger">Opret 👤</NuxtLink>
+          <NuxtLink to="Abboner">🔔 Abboner</NuxtLink>
         </li>
       </ul>
     </nav>
+    <li>
+      <NuxtLink class="opret" to="/opret-bruger">
+        <img src="/img/login-ikon.png" alt="" /> Opret bruger</NuxtLink
+      >
+    </li>
   </header>
 </template>
 
 <style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #2e4e2d;
+  padding: 1.5rem 3rem;
+  list-style: none;
+}
+
+.logo img {
+  max-width: 80px;
+}
+
 .nav-list {
   display: flex;
-  justify-content: flex-end;
   gap: 4rem;
-  padding-right: 100px;
-  background-color: green;
-  padding: 2rem;
-}
-
-ul {
   list-style: none;
-  padding: 0;
-  margin: 0;
 }
 
-a {
+.nav-list a {
   color: white;
+  text-decoration: none;
+  font-size: 1.2rem;
+}
+
+.nav-list a:hover {
+  text-decoration: underline;
+}
+
+.opret {
+  display: inline-flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 1rem 1rem;
+  border-radius: 25px;
+  background-color: #dd9b21;
+  color: black;
+  font-weight: 600;
   text-decoration: none;
 }
 
-a:hover {
-  text-decoration: underline;
+.opret img {
+  width: 18px;
+  height: 18px;
+  position: relative;
+  top: 2px;
 }
 </style>
---
+++ mit-nuxt-projekt/app/pages/abboner.vue
@@ -0,0 +1,1 @@
+<template>abboner</template>
