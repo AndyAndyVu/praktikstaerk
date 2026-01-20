209c420fc2b8a687112795d3aacbdf946b494382
Andy <andyandyvu@gmail.com>
2026-01-20 11:43:45 +0100

    add om-os siden som component til forside

---
+++ mit-nuxt-projekt/app/components/omos.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="omOs">
+    <img src="../img/bestik.png" alt="gaffel ske" />
+    <div class="text-box">
+      <h1>OM OS</h1>
+      <p>
+        Med vores app vælger du selv dine favoritretter, hvor ofte du vil have
+        dem, og kan justere eller sætte på pause når som helst. Nyd lækker,
+        varieret mad uden planlægning, stress eller tidsforbrug - alt kører helt
+        af sig selv.
+      </p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.omOs {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #638250;
+  padding-top: 5rem;
+  padding-bottom: 5rem;
+  gap: 2rem;
+}
+
+.omOs img {
+  max-width: 400px;
+  width: 100%;
+  height: auto;
+  border-radius: 100px;
+}
+
+h1 {
+  font-size: 4rem;
+  font-weight: bold;
+}
+
+.text-box {
+  margin-left: 2rem;
+  max-width: 30ch;
+}
+</style>
--- mit-nuxt-projekt/app/pages/index.vue
+++ mit-nuxt-projekt/app/pages/index.vue
@@ -4,5 +4,6 @@
     <Btn label="START DIT <br>ABONNOMENT" to="#" />
     <Btn label="START DIT ABONNOMENT" to="#" />
     <Btn label="SE RESTAURANTER" to="#" />
+    <omos />
   </main>
 </template>
