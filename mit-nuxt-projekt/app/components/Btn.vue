58f27381f9e63b764178d9baadc5a7b1f6add9e8
Andy <andyandyvu@gmail.com>
2026-01-20 11:10:12 +0100

    fix label så den render til v-html så man kan <br> i  btn label

--- mit-nuxt-projekt/app/components/Btn.vue
+++ mit-nuxt-projekt/app/components/Btn.vue
@@ -1,1 +1,30 @@
-<template>test</template>
+<template>
+  <NuxtLink :to="to" class="knap">
+    <span v-html="label"></span>
+  </NuxtLink>
+</template>
+
+<script setup>
+defineProps({
+  label: String,
+  to: String,
+});
+</script>
+
+<!-- defineProps anvendes for at gøre knappen genbrugelig.
+label bestemmer teksten i knappen,
+to angiver hvilken side knappen linker til. -->
+
+<style scoped>
+.knap {
+  text-align: center;
+  display: inline-block;
+  padding: 1rem 1rem;
+  border-radius: 1.5rem;
+  background-color: #ffcc52;
+  margin-right: 2rem;
+  color: black;
+  text-decoration: none;
+  font-weight: bold;
+}
+</style>
--- mit-nuxt-projekt/app/pages/index.vue
+++ mit-nuxt-projekt/app/pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-  <Navdesk />
+  <main>
+    <Navdesk />
+    <Btn label="START DIT <br>ABONNOMENT" to="#" />
+    <Btn label="START DIT ABONNOMENT" to="#" />
+    <Btn label="SE RESTAURANTER" to="#" />
+  </main>
 </template>
